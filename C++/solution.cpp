82c4f91af72c44e1b0f698a9e6e877f52e6f6994
5H4N3O <shanepratt2002@gmail.com>
2025-01-12 20:27:03 -0600

    Added more test scenarios

--- C++/solution.cpp
+++ C++/solution.cpp
@@ -22,13 +22,48 @@ public:
 
 int main() {
     Solution exampleTest;
-    vector<int> nums1 {1,2,3,0,0,0}; 
-    int m = 3; 
-    vector<int> nums2 {2,5,6}; 
-    int n = 3;  
+    vector<int> nums1; 
+    int m; 
+    vector<int> nums2; 
+    int n;
+
+    // Example 1
+    nums1 = {1,2,3,0,0,0};
+    m = 3;
+    nums2 = {2,5,6};
+    n = 3;
     exampleTest.merge(nums1, m, nums2, n);
+    cout << "Example 1:" << endl;
+
     for(int i=0; i< nums1.size(); i++){
         cout << nums1[i];
     } 
+    cout << endl;
+
+    // Example 2
+    nums1 = {1};
+    m = 1;
+    nums2 = {};
+    n = 0;
+    exampleTest.merge(nums1, m, nums2, n);
+    cout << "Example 2:" << endl;
+
+    for(int i=0; i< nums1.size(); i++){
+        cout << nums1[i];
+    } 
+    cout << endl;
+
+    // Example 3
+    nums1 = {0};
+    m = 0;
+    nums2 = {1};
+    n = 1;
+    exampleTest.merge(nums1, m, nums2, n);
+    cout << "Example 3:" << endl;
+
+    for(int i=0; i< nums1.size(); i++){
+        cout << nums1[i];
+    } 
+    cout << endl;
 
 }
